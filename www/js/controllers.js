6b61cd96a082ca599c1bcd7bfa0236d247a70d82
xuting715 <949330215@qq.com>
2015-12-28 15:34:17 +0800

    1.format date 2.add refresh 3.add download logic

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,7 +1,15 @@
 angular.module('starter.controllers', [])
 
-  .controller('DashCtrl', ['$scope', 'Solutions', function ($scope, Solutions) {
+  .controller('DashCtrl', ['$scope', 'Solutions','$rootScope','$cordovaDialogs', function ($scope, Solutions,$rootScope, $cordovaDialogs) {
     $scope.solutions = Solutions.query();
+    $scope.doRefresh = function(){
+      $scope.solutions = Solutions.query(function(){
+        $scope.$broadcast('scroll.refreshComplete');
+      })
+    }
+    $scope.download_apk = function (platforms,currentPlatform) {
+      download(platforms,currentPlatform,$rootScope,$cordovaDialogs);
+    }
   }])
 
   .controller('CategoriesCtrl', function ($scope) {
@@ -31,3 +39,29 @@ angular.module('starter.controllers', [])
     };
   })
 
+// download apk
+function download(platforms,currentPlatform,$rootScope,$cordovaDialogs){
+  var flag = true;
+  var platformsStr = "";
+  for(var i =0;i <platforms.length;i++){
+    platformsStr =platformsStr+ platforms[i].name+",";
+    if(platforms[i].name.toLowerCase().trim() == currentPlatform){
+      flag = false;
+      window.open($rootScope.url+platforms[i].url, "_system", "location=yes,enableViewportScale=yes,hidden=no");
+    }
+  }
+
+  if(flag){
+    var message ='The platform is not support,only support '+platformsStr+' do you want continue to download ?';
+    $cordovaDialogs.confirm(message, 'title', ['OK','Cancel'])
+      .then(function(buttonIndex) {
+        // no button = 0, 'OK' = 1, 'Cancel' = 2
+        var btnIndex = buttonIndex;
+        if(btnIndex == 1){
+          //download
+          window.open($rootScope.url+platforms[0].url, "_system", "location=yes,enableViewportScale=yes,hidden=no");
+        }
+      });
+
+  }
+}
